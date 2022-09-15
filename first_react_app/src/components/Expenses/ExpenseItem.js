ff3e21519260ba75aaf9170c7b8062137c69c9b0
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2022-09-15 15:11:08 +0600

    useState, updateState, two way binding.

--- first_react_app/src/components/Expenses/ExpenseItem.js
+++ first_react_app/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+
+    console.log("ExpenseItem evaluated by react!");
+
+    const clickHandler = () => {
+        setTitle("updated!!");
+        console.log(title);
+    };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
