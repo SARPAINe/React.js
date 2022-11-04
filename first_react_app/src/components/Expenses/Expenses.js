9a17e64280a4ca7542c1c597a1e2e43b1e583466
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2022-11-04 11:24:05 +0600

    Sec 5: Rendering lists and conditional Content

--- first_react_app/src/components/Expenses/Expenses.js
+++ first_react_app/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
     const [selectedYear, setSelectedYear] = useState(2019);
     const filterChangeHandler = (chosenYear) => {
@@ -20,6 +21,7 @@ const Expenses = (props) => {
                     onChangeFilter={filterChangeHandler}
                     selected={selectedYear}
                 ></ExpensesFilter>
+                <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
                 <ExpensesList items={filteredExpenses}></ExpensesList>
             </Card>
         </div>
