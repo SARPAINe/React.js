fc8c5636e5b36299530391e6542fdd19625d16c9
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2022-12-09 22:00:54 +0600

    Section 8 practise project completed

---
+++ 7_first_practise_project/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
+
+function App() {
+    // const users = [
+    //     { name: "refat", age: 24 },
+    //     { name: "shammy", age: 30 },
+    // ];
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (uName, uAge) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                { name: uName, age: uAge, id: Math.random().toString() },
+            ];
+        });
+    };
+
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler}></AddUser>;
+            <UsersList users={usersList}></UsersList>
+        </div>
+    );
+}
+
+export default App;
