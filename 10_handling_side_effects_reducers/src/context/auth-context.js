6c9fb4a201c218db8fa68ab935eef93c5622930a
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2022-12-29 00:02:56 +0600

    Added input component

--- 10_handling_side_effects_reducers/src/context/auth-context.js
+++ 10_handling_side_effects_reducers/src/context/auth-context.js
@@ -2,6 +2,7 @@ import React from "react";
 //AuthContext is an object that will contain a component
 const AuthContext = React.createContext({
     isLoggedIn: false,
+    onLogout: () => {},
 });
 
 export default AuthContext;
